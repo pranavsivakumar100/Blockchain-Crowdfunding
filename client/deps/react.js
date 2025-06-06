a5cb60b008d90f50bce8e081e2295d1debc3720d
pranavsivakumar <pranav.sivakumar100@gmail.com>
2025-06-06 13:48:19 -0400

    change

---
+++ client/deps/react.js
@@ -0,0 +1,6 @@
+import {
+  require_react
+} from "./chunk-IHAKEAXL.js";
+import "./chunk-OLZF3EOE.js";
+export default require_react();
+//# sourceMappingURL=react.js.map
--- client/node_modules/@thirdweb-dev/react/dist/declarations/src/components/LoadingScreen.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @internal
- */
-export declare function LoadingScreen(props: {
-    height?: string;
-}): import("react/jsx-runtime").JSX.Element;
-//# sourceMappingURL=LoadingScreen.d.ts.map
